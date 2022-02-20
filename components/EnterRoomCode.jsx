574453affcaafa3e11eb377b65a1698c4e931af1
Bruno Botelho <brunorabelow@gmail.com>
2022-02-20 01:43:15 +0100

    keyboard dismiss

--- components/EnterRoomCode.jsx
+++ components/EnterRoomCode.jsx
@@ -1,4 +1,14 @@
-import { StyleSheet, View, TextInput, Text, Alert, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  Alert,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 import { useEffect, useState } from "react";
 
@@ -28,32 +38,40 @@ export default function EnterRoomCode({ getName, setRoomId }) {
   // useEffect(() => setId(Id.toUpperCase()), [Id])
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Enter room code:</Text>
-      <View style={styles.searchSection}>
-        <TextInput
-          placeholder="A1B2C3"
-          style={[styles.textInputStyle, styles.input]}
-          onChangeText={setId}
-        ></TextInput>
-        <View style={styles.inputIcon}>
-          <Image
-            style={styles.qrcodeIcon}
-            source={require("../assets/icons/007-qr-code-read.png")}
-            name="ios-search"
-            size={10}
-          />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Enter room code:</Text>
+        <View style={styles.searchSection}>
+          <TextInput
+            placeholder="A1B2C3"
+            style={styles.textInputStyle}
+            onChangeText={setId}
+          ></TextInput>
+          <View style={styles.inputIcon}>
+            <TouchableOpacity
+              onPress={() => {
+                console.log("Pressed");
+              }}
+            >
+              <Image
+                style={styles.qrcodeIcon}
+                source={require("../assets/icons/007-qr-code-read.png")}
+                name="ios-search"
+                size={10}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
 
-      <Button
-        label="Ok"
-        action={handleSubmit}
-        backgroundColor={colors.blue}
-        textColor={colors.dark}
-      />
-      <View style={styles.space} />
-    </View>
+        <Button
+          label="Ok"
+          action={handleSubmit}
+          backgroundColor={colors.blue}
+          textColor={colors.dark}
+        />
+        <View style={styles.space} />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -67,6 +85,7 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     paddingVertical: 20,
     flex: 1,
+    height: 40,
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",

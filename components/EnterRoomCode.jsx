8fbc1fb1fc518381508a0997801193ea7c49db26
Bruno Botelho <brunorabelow@gmail.com>
2022-02-21 01:58:22 +0100

    now keyboard doesnt overlay GetName

--- components/EnterRoomCode.jsx
+++ components/EnterRoomCode.jsx
@@ -35,6 +35,7 @@ export default function EnterRoomCode({
       Alert.alert("Room name invalid.", "Please enter a valid room name.");
       return;
     }
+    Keyboard.dismiss()
     setRoomId(Id.toUpperCase());
     getName();
   };

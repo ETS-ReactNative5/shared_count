6d7ebfb63f43ceb09cdacb7be79f2a3ec79005f9
guilevieiram <guilhermevmanhaes@gmail.com>
2022-02-21 17:27:24 +0100

    reestart button on NotConnected page

--- pages/NotConnected.jsx
+++ pages/NotConnected.jsx
@@ -1,10 +1,20 @@
+// import RNRestart from 'react-native-restart';
+import {Restart} from 'fiction-expo-restart';
+
 import {
     View, 
     Text,
     StyleSheet,
 } from "react-native"
 
+import {
+    Button
+} from "../components/elements";
+
+import config from '../config';
+
 export default function NotConnected () {
+    const handleRestart = () => Restart();
     return (
         <View style={styles.container}> 
             <Text style={styles.title}>
@@ -13,6 +23,12 @@ export default function NotConnected () {
             <Text style={styles.message}>
                 Try checking your Wi-fi or mobile network connection!
             </Text>
+            <Button 
+                label="Restart app." 
+                action={handleRestart}
+                backgroundColor={config.colors.blue}
+                textColor={config.colors.dark}
+            />
         </View>
     )
 }
